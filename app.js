448bb95c18ceb9d9a01fac929a093d020ba6b9ea
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2023-03-01 11:21:30 +0530

    chore:vibration effect removed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
- window.navigator.vibrate(600);
+ 
     hideMessage();
 
      if (billAmount.value >  0){
